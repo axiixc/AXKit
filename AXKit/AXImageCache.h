7d01c219991ab6fdfacdb7b9cf6fac76f97477ad
James Savage <axiixc@gmail.com>
2011-12-09 20:52:12 -0500

    Reorganizing Project Workspace

---
+++ AXKit/AXImageCache.h
@@ -0,0 +1,11 @@
+//
+//  AKImageCache.h
+//  AXKit
+//
+//  Created by James Savage on 12/9/11.
+//  Copyright (c) 2011 axiixc.com. All rights reserved.
+//
+
+@interface AXImageCache : NSObject
+
+@end
---
+++ AXKit/AXImageCache.m
@@ -0,0 +1,13 @@
+//
+//  AKImageCache.m
+//  AXKit
+//
+//  Created by James Savage on 12/9/11.
+//  Copyright (c) 2011 axiixc.com. All rights reserved.
+//
+
+#import "AXImageCache.h"
+
+@implementation AXImageCache
+
+@end
