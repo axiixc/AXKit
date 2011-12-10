db6abd704d2d718776d4465c8a71eafee754cb6f
James Savage <axiixc@gmail.com>
2011-12-09 20:45:21 -0500

    Initial Commit

---
+++ AXKit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AXKit
+//
+//  Created by James Savage on 12/9/11.
+//  Copyright (c) 2011 axiixc.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AXKit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AXKit
+//
+//  Created by James Savage on 12/9/11.
+//  Copyright (c) 2011 axiixc.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
